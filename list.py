543d505cb1307a8a166709c7f9dc48784dd23697
chi_intern <buiminhchi0405@gmail.com>
2022-04-13 08:09:32 +0000

    g

--- list.py
+++ list.py
@@ -6,10 +6,10 @@ print("the reverse list ", list1)
 
 list2 = [1,2,33,100,5,6,77,8,9,]
 list2.sort()
-print("the sort list", list2)
+print("the sort list2", list2)
 
 list3 = list1.copy()
 print("list3 = ", list3)
 
-ind = list2[2,5]
+ind = list2[2:5]
 print("is ", ind)
